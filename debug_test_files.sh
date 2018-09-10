f6f89f55f36524774b51de2fc0d2dbc14e042edd
Kareem Straker <kareem.straker@gmail.com>
2018-09-10 15:24:18 +0200

    Remove unused debug file

--- debug_test_files.sh
+++
@@ -1,7 +0,0 @@
-## Make test files
-mkdir -p config/ssl
-touch config/ssl/ca.pem
-touch config/ssl/api-server.pem
-touch config/ssl/api-server-key.pem
-touch config/ssl/worker.pem
-touch config/ssl/worker-key.pem
